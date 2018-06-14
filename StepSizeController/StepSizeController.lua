f844275bca22d30a8bd3339af353b69a952a33ab
Rebeka Farkas <rebus.detenyleg@gmail.com>
2018-06-14 14:14:44 +0200

    Add step size control for chains of discrete events

--- StepSizeController/StepSizeController.lua
+++ StepSizeController/StepSizeController.lua
@@ -32,37 +32,63 @@ SensitivityModel.__index = SensitivityModel
 function SensitivityModel:create()
 	local sm = {}
 	setmetatable(sm,SensitivityModel)
-	sm.events = {} -- discrete events - collection of variables, where any change is interesting
+	sm.events = {} -- discrete events - set of variables, where any change is interesting
 	sm.zeroCrossings = {} -- maps from variable name to step size adjustment based on value
-	-- sm.monitors -- todo: predictive monitors
 	return sm
 end
 
 -- step size control based on a sensitivity model
-function getNextStepSize(sensitivityModel,communicationInterval)
-	local minStepSize=communicationInterval
-	-- discrete chains
-	-- tbd	
-	-- zero crossings
-	local zc=sensitivityModel.zeroCrossings
-	for variable,bandFunc in pairs(zc)
+function didEventOccur(vars)
+	local eventOccured=false
+	for v,_ in pairs(vars)
 	do
-		value=oms2_getReal(k)
-		step=bandFunc(value)
-		if (step != nil and step<minStepSize) then
-			minStepSize=step
-		end
+		local prevVal=values[v]	
+		local currVal=oms2_getReal(v)
+		if (prevVal != currVal) then
+			values[v]=currVal
+			eventOccured=true
+		end	
 	end
-	-- predictive models
-	-- tbd
-	return minStepSize
+	return eventOccured
+end
+
+function getValues(vars)
+	local values={}
+	for v,_ in pairs(vars)
+	do
+		values[v]=oms2_getReal(v)	
+	end
+	return values
+end
+
+function getNextStepSize(prevValues,zeroCrossings,communicationInterval)
+	local minStepSize=communicationInterval
+	-- discrete event chains: if there was an event we adjust the step size to the minimal value
+	local eventOccured=didEventOccur(prevValues)
+	if (eventOccured) then
+		minStepSize=0.001
+		return minStepSize
+	else
+		-- zero crossings
+		local zc=zeroCrossings
+		for variable,bandFunc in pairs(zc)
+		do
+			value=oms2_getReal(k)
+			step=bandFunc(value)
+			if (step != nil and step<minStepSize) then
+				minStepSize=step
+			end
+		end
+		return minStepSize
+	end
 end
 
 function oms2_simulateWithASSC(model,communicationInterval,sensitivityModel,tmax)
 	local tcur=0.0
+	local prevValues=getValues(sensitivityModel.events)
 	while (tcur<tmax)
 	do
-		local nextStepSize = getNextStepSize(sensitivityModel,communicationInterval)
+		local nextStepSize = getNextStepSize(prevValues,sensitivityModel.zeroCrossings,communicationInterval)
 		oms2_setCommunicationInterval(model, nextStepSize)
 		oms2_doSteps(model,1)
 		tcur=tcur+nextStepSize
