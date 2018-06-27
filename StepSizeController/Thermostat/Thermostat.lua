62856eb1c7e1d7a9eeee1b808dda4fd5271e641b
Rebeka Farkas <rebus.detenyleg@gmail.com>
2018-06-27 15:13:57 +0200

    Add fmus for Thermostat case study

--- StepSizeController/Thermostat/Thermostat.lua
+++ StepSizeController/Thermostat/Thermostat.lua
@@ -1,11 +1,11 @@
-oms2_setLogFile("TrafficLightScenario.log")
-oms2_newFMIModel("ThermostatExample")
-oms2_setTempDirectory("./tmp")
-oms2_addFMU("ThermostatExample", "advancedThermostat.fmu", "Thermostat")
-oms2_setResultFile("ThermostatExample", "Thermostat.mat")
+--[[
+package.path = package.path .. ";../StepSizeController.lua"
+require("StepSizeController")
+--]]
 
-oms2_initialize("ThermostatExample")
-offint=oms2_getInteger("ThermostatExample.Thermostat:Thermostat_HeatingControlPort_turn_heating_off")
-print(offint)
-oms2_terminate("ThermostatExample")
-oms2_unloadModel("ThermostatExample")
+
+oms2_setLogFile("Thermostat.log")
+oms2_setTempDirectory("./tmp")
+oms2_newFMIModel("ThermostatExample")
+
+oms2_addFMU("ThermostatExample","advancedThermostat.fmu","Thermostat")
