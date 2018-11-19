7a5450d5868e578932c772bb6d7a421aadc0d6aa
Rebeka Farkas <rebus.detenyleg@gmail.com>
2018-11-19 14:12:46 +0100

    Fix identation

--- src/OMSimulatorLib/StepSizeConfiguration.h
+++ src/OMSimulatorLib/StepSizeConfiguration.h
@@ -7,43 +7,43 @@
 
 //auxiliary data structures
 struct staticBound {
-    double lower;
-    double upper;
-    double stepSize;
+  double lower;
+  double upper;
+  double stepSize;
 };
 
 struct dynamicBound {
-    const oms2::SignalRef lower;
-    const oms2::SignalRef upper;
-    double stepSize;
+  const oms2::SignalRef lower;
+  const oms2::SignalRef upper;
+  double stepSize;
 };
 
 class StepSizeConfiguration {
-    public:
-        StepSizeConfiguration();
-        ~StepSizeConfiguration();
+  public:
+    StepSizeConfiguration();
+    ~StepSizeConfiguration();
         
-        //data structure class - only getters and setters
-        void setMinimalStepSize(double min) {minimalStepSize=min;};
-        double getMinimalStepSize() {return minimalStepSize;};
-        void setMaximalStepSize(double max) {maximalStepSize=max;};
-        double getMaximalStepSize() {return maximalStepSize;};
-        void addEventIndicator(const oms2::SignalRef& signal);
-        std::vector<oms2::SignalRef> getEventIndicators() {return eventIndicators;};
-        void addTimeIndicator(const oms2::SignalRef& signal);
-        std::vector<oms2::SignalRef> getTimeIndicators() {return timeIndicators;};
-        void addStaticValueIndicator(const oms2::SignalRef& signal, double lowerBound, double upperBound, double stepSize);
-        void addDynamicValueIndicator(const oms2::SignalRef& signal,const oms2::SignalRef& lower,const oms2::SignalRef& upper,double stepSize);
-        std::vector<std::pair<const oms2::SignalRef,std::vector<staticBound>>> getStaticThresholds() {return staticIntervals;};
-        std::vector<std::pair<const oms2::SignalRef,std::vector<dynamicBound>>> getDynamicThresholds(){return dynamicIntervals;};
+    //data structure class - only getters and setters
+    void setMinimalStepSize(double min) {minimalStepSize=min;};
+    double getMinimalStepSize() {return minimalStepSize;};
+    void setMaximalStepSize(double max) {maximalStepSize=max;};
+    double getMaximalStepSize() {return maximalStepSize;};
+    void addEventIndicator(const oms2::SignalRef& signal);
+    std::vector<oms2::SignalRef> getEventIndicators() {return eventIndicators;};
+    void addTimeIndicator(const oms2::SignalRef& signal);
+    std::vector<oms2::SignalRef> getTimeIndicators() {return timeIndicators;};
+    void addStaticValueIndicator(const oms2::SignalRef& signal, double lowerBound, double upperBound, double stepSize);
+    void addDynamicValueIndicator(const oms2::SignalRef& signal,const oms2::SignalRef& lower,const oms2::SignalRef& upper,double stepSize);
+    std::vector<std::pair<const oms2::SignalRef,std::vector<staticBound>>> getStaticThresholds() {return staticIntervals;};
+    std::vector<std::pair<const oms2::SignalRef,std::vector<dynamicBound>>> getDynamicThresholds(){return dynamicIntervals;};
 
-    private:
-        double minimalStepSize;
-        double maximalStepSize;
-        std::vector<oms2::SignalRef> eventIndicators;
-        std::vector<oms2::SignalRef> timeIndicators;
-        std::vector<std::pair<const oms2::SignalRef,std::vector<staticBound>>> staticIntervals;
-        std::vector<std::pair<const oms2::SignalRef,std::vector<dynamicBound>>> dynamicIntervals;
+  private:
+    double minimalStepSize;
+    double maximalStepSize;
+    std::vector<oms2::SignalRef> eventIndicators;
+    std::vector<oms2::SignalRef> timeIndicators;
+    std::vector<std::pair<const oms2::SignalRef,std::vector<staticBound>>> staticIntervals;
+    std::vector<std::pair<const oms2::SignalRef,std::vector<dynamicBound>>> dynamicIntervals;
 };
 
 #endif
